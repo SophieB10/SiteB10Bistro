61b50c17f734139597dcf86918d1cb197d386393
SophieB10 <sp@blue10.com>
2022-09-28 15:25:43 +0200

    small

--- Function/TableOutput.cs
+++ Function/TableOutput.cs
@@ -68,7 +68,7 @@ namespace MenuFunctionOutput
                 if(!valid) return new BadRequestObjectResult("Order is null 😢");
             }
             catch(Exception e){
-                var msg = $"Hepl! Could not deserialize msg: {req.Body}";
+                var msg = $"Hepl! Could not deserialize msg: {reqBody}";
                 log.LogError(e,msg);
                 return new BadRequestObjectResult(msg);
             }             
